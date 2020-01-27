020e81e7d92faf9374b6b4b4f09e2e7ddb405f4d
david-pok <david.pok@gmail.com>
2020-01-27 15:48:38 -0800

    part 1 done, working on part2

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
+  console.log("MOVIE PROPS", props)
   const [movie, setMovie] = useState();
  
   useEffect(() => {
